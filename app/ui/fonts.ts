433d74b36b2c141657df06390bdaed277e209ee2
rickplumeria <48343274+rickplumeria@users.noreply.github.com>
2023-12-10 12:53:35 +0000

    feat: done chapter 3

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,4 +1,8 @@
-import { Noto_Sans, Roboto } from 'next/font/google';
+import { Lusitana, Noto_Sans, Roboto } from 'next/font/google';
 
 export const noto_sans = Noto_Sans({ subsets: ['latin'] });
 export const roboto = Roboto({ subsets: ['latin'], weight: ['400', '700'] });
+export const lusitana = Lusitana({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+});
